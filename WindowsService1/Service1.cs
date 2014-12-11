2f8354534b180c2492af037e67fdda3cb3d43541
Mike Herrera <mike@devoplabs.com>
2014-12-10 20:20:18 -0800

    initial commit

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,86 @@
+/*
+ *  Michael Herrera (michael.herrera@gmail.com)
+ * 
+ */
+
+using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Threading;
+
+using snapService;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+
+        // ------------------------------------------------------------------------------
+
+        /* EventID convention */
+        internal enum lg_CamEventID : int
+        {
+            // Service IDs (1xxx)
+            Start    = 1000,
+            Stop     = 1001,
+            Shutdown = 1002,
+
+            // Application IDs (2xxx)
+            ThreadStarted = 2000,
+
+            // Error IDs (3xxx)
+            ErrorJpegDownload = 3000,
+            ZipNotFound = 3001,
+
+            // Debug IDs (9xxx)
+            DebugGeneric = 9000,
+        }
+
+        // ------------------------------------------------------------------------------
+
+        Thread worker = null;
+
+        public Service1()
+        {
+            InitializeComponent();
+
+            eventLog1.Source = "JpegHarvest";
+            eventLog1.Log = "axis_snap";
+            
+            if (!System.Diagnostics.EventLog.SourceExists(eventLog1.Source))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(eventLog1.Source, eventLog1.Log);
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Service STARTED", 
+                EventLogEntryType.SuccessAudit,
+                (int)lg_CamEventID.Start);
+            
+            worker = new Thread(new ThreadStart(snapService.CWorker.harvestThreadAgent));
+            worker.Start();
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Service STOPPED", 
+                EventLogEntryType.SuccessAudit, 
+                (int)lg_CamEventID.Stop);
+            worker.Abort();
+        }
+
+        protected override void OnShutdown()
+        {
+            eventLog1.WriteEntry("Service STOPPED (Shutdown)", 
+                EventLogEntryType.SuccessAudit,
+                (int)lg_CamEventID.Shutdown);
+            worker.Abort();
+        }
+
+        // ------------------------------------------------------------------------------
+
+    } // class Service1
+
+} // namespace WindowsService1
---
+++ WindowsService1/axis_snap.cs
@@ -0,0 +1,263 @@
+/*
+ *  Michael Herrera (michael.herrera@gmail.com)
+ * 
+ */
+
+using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Timers;
+
+namespace snapService
+{
+    class CWorker
+    {
+        // ------------------------------------------------------------------------------
+
+        #region Main working thread
+
+
+        internal static void harvestThreadAgent()
+        {
+            log = new System.Diagnostics.EventLog();
+            log.Source = "JpegHarvest";
+            log.Log = "axis_snap";
+
+            /* SERVICE Config */
+            int _sleepInterval = int.Parse(ConfigurationManager.AppSettings["mainThreadSleep_ms"]);
+
+            /* CAMERAS Config */
+            _numCameras = int.Parse(ConfigurationManager.AppSettings["numCameras"]);
+
+            /* Capture Parameters */
+            _captureIntervalInSeconds = int.Parse(ConfigurationManager.AppSettings["captureIntervalInSeconds"]);
+
+            /* LOCAL ARCHIVE Config */
+            _localArchiveRoot = ConfigurationManager.AppSettings["localArchiveRoot"];
+
+            /* ZIP Config */
+            _zipArchiveDuration_hr = int.Parse(ConfigurationManager.AppSettings["zip_DurationPerArchive_hr"]);
+
+            _cameraJpegPrefix = "AxisCamera";
+            _cameraJpegSuffix = "_jpg";
+            _cameraLogicalIDPrefix = "AxisCamera";
+            _cameraLogicalIDSuffix = "_logicalID";
+            _cameraCommonNameSuffix = "_commonName";
+
+            _jpegNamingPrefix = "cam_";
+
+            Timer jpegHarvestTimer = new Timer();
+            jpegHarvestTimer.AutoReset = false;
+            jpegHarvestTimer.Elapsed += new ElapsedEventHandler(jpegHarvestTimer_Elapsed);
+            
+            while (true)
+            {
+                if (!jpegHarvestTimer.Enabled)
+                {
+                    jpegHarvestTimer.Interval = ((60 - DateTime.Now.Second) % _captureIntervalInSeconds) * 1000;
+                    jpegHarvestTimer.Start();
+                }
+
+                System.Threading.Thread.Sleep(_sleepInterval);
+            }
+        }
+
+
+        #endregion
+
+        // ------------------------------------------------------------------------------
+
+        #region Timer event handlers
+
+
+        static void jpegHarvestTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            WebClient oClient = new WebClient();
+
+            string dateString = DateTime.Now.ToString("yyyy.MM.dd_HH.mm.ss");
+            string jpegSourceURL;
+            string cameraLogicalID;
+            string cameraCommonName;
+            string jpegDestFilePath;
+
+            for (int idx = 0; idx < _numCameras; idx++)
+            {
+
+                // CAPTURE JPEG -----------------
+
+                jpegSourceURL = ConfigurationManager.AppSettings[_cameraJpegPrefix + idx.ToString() + _cameraJpegSuffix];
+                cameraLogicalID = ConfigurationManager.AppSettings[_cameraLogicalIDPrefix + idx.ToString() + _cameraLogicalIDSuffix];
+                cameraCommonName = ConfigurationManager.AppSettings[_cameraLogicalIDPrefix + idx.ToString() + _cameraCommonNameSuffix];
+                jpegDestFilePath = _localArchiveRoot + _jpegNamingPrefix + cameraCommonName + "_" + dateString + ".jpeg";
+
+                try
+                {
+                    oClient.DownloadFile(jpegSourceURL, jpegDestFilePath);  // 1. Fetch Jpeg from encoder
+
+                    ZipWriter oZipAgent = getZipWriter(idx);                // 2. Store Jpeg in zip (creates zip, if needed)
+                    oZipAgent.BeginUpdate();
+                    oZipAgent.AddFileUncompressed(jpegDestFilePath);
+                    oZipAgent.CommitUpdate();
+                    oZipAgent.Close();
+
+                    System.IO.File.Delete(jpegDestFilePath);                // 3. Delete Jpeg
+                }
+                catch (Exception ex)
+                {
+                    log.WriteEntry("ERROR (jpegHarvestTimer_Elapsed)\n\n" +
+                        "jpegSourceURL = " + jpegSourceURL + "\n\n" +
+                        "jpegDestFilePath = " + jpegDestFilePath + "\n\n" +
+                        ex.ToString(),
+                        EventLogEntryType.Error, 
+                        (int)WindowsService1.Service1.lg_CamEventID.ErrorJpegDownload);
+                }
+            }
+        }
+
+
+        #endregion
+
+        // ------------------------------------------------------------------------------
+
+        #region Zip Methods
+
+
+        static ZipWriter getZipWriter(int cameraIndex)
+        {
+            int zipArchiveStart = DateTime.Now.Hour - (DateTime.Now.Hour % _zipArchiveDuration_hr);
+            int zipArchiveStop = zipArchiveStart + _zipArchiveDuration_hr;
+
+            // Zip Naming: E.g. cam_LobbyDoor_2007.12.31_0300-0400.zip
+
+            string zipArchiveName = "cam_" +
+                ConfigurationManager.AppSettings["AxisCamera" + cameraIndex + "_commonName"] + "_" +
+                DateTime.Now.ToString("yyyy.MM.dd_") +
+                zipArchiveStart.ToString().PadLeft(2, '0') + "00-" +
+                zipArchiveStop.ToString().PadLeft(2, '0')  + "00.zip";
+
+            return new ZipWriter(_localArchiveRoot + zipArchiveName);
+        }
+
+
+        #endregion
+
+        // ------------------------------------------------------------------------------
+
+        #region Data members
+
+
+        private static EventLog log;
+
+        private static string _cameraJpegPrefix;
+        private static string _cameraJpegSuffix;
+
+        private static string _cameraLogicalIDPrefix;
+        private static string _cameraLogicalIDSuffix;
+        private static string _cameraCommonNameSuffix;
+
+        private static int _captureIntervalInSeconds;
+
+        private static int _zipArchiveDuration_hr;
+
+        private static string _jpegNamingPrefix;
+        private static string _localArchiveRoot;
+        private static int _numCameras;
+
+
+        #endregion
+
+    } // class CWorker
+
+    // ----------------------------------------------------------------------------------
+
+    #region ZIP CLASSES
+
+
+    public class ZipDataSource : IStaticDataSource
+    {
+        string filename;
+
+        public ZipDataSource(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public System.IO.Stream GetSource()
+        {
+            return System.IO.File.OpenRead(filename);
+        }
+    
+    } // class ZipDataSource
+
+    public class ZipReader
+    {
+        private ZipFile oZipFile;
+        private string pathToZip;
+
+        public ZipReader(string ZipPath)
+        {
+            this.pathToZip = ZipPath;
+            if (!System.IO.File.Exists(this.pathToZip))
+            {
+                EventLog log = new System.Diagnostics.EventLog();
+                log.Source = "JpegHarvest";
+                log.Log = "axis_snap";
+
+                log.WriteEntry("ZipReader: " + pathToZip + " is not available.",
+                    EventLogEntryType.Warning, 
+                    (int)WindowsService1.Service1.lg_CamEventID.ZipNotFound);
+            }
+            else
+            {
+                oZipFile = new ZipFile(this.pathToZip);
+            }
+        }
+
+        public void ExtractZip(string targetDir)
+        {
+            FastZip oFastZip = new FastZip();
+            oFastZip.ExtractZip(this.pathToZip, targetDir, FastZip.Overwrite.Always, null, "", "", true);
+        }
+
+        public ZipFile getZipFile() { return oZipFile; }
+    
+    } // class ZipReader
+
+    public class ZipWriter
+    {
+        private ZipFile oZipFile;
+        private string pathToZip;
+
+        public ZipWriter(string ZipPath)
+        {
+            this.pathToZip = ZipPath;
+            if (!System.IO.File.Exists(this.pathToZip))
+            {
+                oZipFile = ZipFile.Create(this.pathToZip);
+            }
+            else
+            {
+                oZipFile = new ZipFile(this.pathToZip);
+            }
+        }
+
+        public void AddFileUncompressed(string filePath)
+        {
+            oZipFile.Add(new ZipDataSource(filePath),
+                         Path.GetFileName(filePath),
+                         CompressionMethod.Stored,
+                         true);
+        }
+
+        public void BeginUpdate()  { oZipFile.BeginUpdate();  }
+        public void Close()        { oZipFile.Close();        }
+        public void CommitUpdate() { oZipFile.CommitUpdate(); }
+    
+    } // class ZipWriter
+
+
+    #endregion
+}
